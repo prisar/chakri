f27220fc6eb83496d11ea9a9b563d2485df7935a
prisar <pritimaysarkar2010@gmail.com>
2022-02-05 00:03:41 +0530

    show bottom nav bar

--- app/src/main/java/com/prisar/chakri/MainActivity.kt
+++ app/src/main/java/com/prisar/chakri/MainActivity.kt
@@ -3,7 +3,10 @@ package com.prisar.chakri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -11,8 +14,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val navView: NavigationView = findViewById(R.id.nav_view)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
         val navController = findNavController(R.id.nav_host_fragment)
+        val appBarConfiguration = AppBarConfiguration(setOf(
+//            R.id.welcome_fragment,
+            R.id.quiz_fragment))
+//        setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
 }
