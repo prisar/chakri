b2340a3eeb2f118ffa654cedf0e2f6b27bfe7c7e
prisar <prisar230@gmail.com>
2023-01-14 18:50:16 +0530

    s

---
+++ app/src/main/java/com/prisar/chakri/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.prisar.chakri.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val White = Color(0xFFFFFFFF)
+val Black = Color(android.graphics.Color.BLACK)
+val Yellow = Color(red = 0xFB, green = 0xB8, blue = 0x0B, alpha = 0xFF)
