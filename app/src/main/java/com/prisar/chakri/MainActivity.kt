bd5ffbe0eba8c04af0a32489083989e8270520bb
prisar <prisar230@gmail.com>
2023-01-15 21:53:47 +0530

    speech

--- app/src/main/java/com/prisar/chakri/MainActivity.kt
+++ app/src/main/java/com/prisar/chakri/MainActivity.kt
@@ -4,11 +4,24 @@ import android.os.Bundle
 import android.speech.tts.TextToSpeech
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults.buttonColors
+import androidx.compose.material.Card
 import androidx.compose.material.Text
+import androidx.compose.material.contentColorFor
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.prisar.chakri.ui.theme.ChakriTheme
+import com.prisar.chakri.ui.theme.Teal200
 import java.util.*
 
 class MainActivity : ComponentActivity() {
@@ -26,14 +39,45 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text("Chakri")
+fun CardWithMultipleViews() {
+    val paddingModifier = Modifier.padding(10.dp)
+    Card(
+        elevation = 10.dp,
+        contentColor = Color.White,
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(400.dp)
+            .padding(10.dp)
+            .background(color = Teal200)
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            val textToSpeech: TextToSpeech = TextToSpeech(LocalContext.current, TextToSpeech.OnInitListener {  })
 
-    val textToSpeech: TextToSpeech = TextToSpeech(LocalContext.current, TextToSpeech.OnInitListener {  })
+            textToSpeech.language = Locale("en", "IN")
 
-    textToSpeech.setLanguage(Locale("bn_IN"))
 
-    Button(onClick = { textToSpeech.speak("তোমার বাড়ি কোথায়", TextToSpeech.QUEUE_FLUSH,null) }) {
-        Text(text = "speak")
+            Button(colors = buttonColors(backgroundColor = Teal200),
+                onClick = { textToSpeech.speak("I", TextToSpeech.QUEUE_FLUSH,null) }) {
+                Text(text = "I", style = TextStyle(fontSize = 24.sp))
+            }
+        }
     }
+}
+
+@Composable
+fun Greeting(name: String) {
+
+    Column(modifier = Modifier.fillMaxWidth(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text("Learn English")
+
+        CardWithMultipleViews()
+    }
+
 }
