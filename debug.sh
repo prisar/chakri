8b027d889610441088602e3a8603946ecaadc9c2
prisar <prisar230@gmail.com>
2023-07-07 08:12:03 +0530

    ip

--- debug.sh
+++ debug.sh
@@ -1,7 +1,13 @@
 adb tcpip 5555
 
-#adb connect 192.168.43.1:5555
+# hotspot
+adb connect 192.168.43.1:5555
 
-adb connect 192.168.0.120:5555
+# pg 1
+# adb connect 192.168.0.120:5555
 
 #adb connect 192.168.1.103:5555
+
+# ofc 1
+#adb connect 192.168.1.16:5555
+
