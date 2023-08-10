92ac28c5c5e49f48ad9824df1ff4c166243ff73b
prisar <prisar230@gmail.com>
2023-08-10 22:09:58 +0530

    add screen rotation

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ screens
 https://www.figma.com/file/McM4TEjycNZeOfOE1FBmCU/chakriapp?node-id=0%3A1
 
 [app](https://play.google.com/store/apps/details?id=com.prisar.chakri)
+
+
+    adb shell content insert --uri content://settings/system --bind name:s:user_rotation --bind value:i:1
+
+
--- debug.sh
+++ debug.sh
@@ -1,13 +1,29 @@
 adb tcpip 5555
 
 # hotspot
-adb connect 192.168.43.1:5555
+#adb connect 192.168.43.1:5555
+
+# adb connect 192.168.8.153:5555
 
 # pg 1
-# adb connect 192.168.0.120:5555
+#adb connect 192.168.0.120:5555
 
-#adb connect 192.168.1.103:5555
+# adb connect 192.168.1.103:5555
 
 # ofc 1
-#adb connect 192.168.1.16:5555
+# adb connect 192.168.1.16:5555
 
+# device 2, pg
+adb connect 192.168.0.123:5555
+
+# device 3, prisar
+#adb connect 192.168.43.79:5555
+
+# device 3, ground floor
+#adb connect 192.168.1.119:5555
+
+# device 3, pg
+#adb connect 192.168.0.152:5555
+
+# mariya, new offc
+#adb connect 192.168.1.5:5555
