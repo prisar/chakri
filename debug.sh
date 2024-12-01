8c83fb30b57e85830ef4fe79c98114c2c778901f
prisar <prisar230@gmail.com>
2024-12-01 23:47:41 +0530

    fix: grade upgrade

--- debug.sh
+++ debug.sh
@@ -1,20 +1,34 @@
 adb tcpip 5555
 
+
+############### phone ############
 # hotspot
-#adb connect 192.168.43.1:5555
+adb connect 192.168.43.1:5555
+#adb connect 192.168.43.170:5555
 
 # adb connect 192.168.8.153:5555
 
-# pg 1
+# modem
+#adb connect 192.168.8.101:5555
+
+# pg 1, iisc
 #adb connect 192.168.0.120:5555
 
 # adb connect 192.168.1.103:5555
 
+# smi ground floor, home top floor
+#adb connect 192.168.1.152:5555
+
 # ofc 1
 # adb connect 192.168.1.16:5555
 
+
+######## other phones ###########
+# smi phone 1, iisc
+#adb connect 192.168.0.100:5555
+
 # device 2, pg
-adb connect 192.168.0.123:5555
+#adb connect 192.168.0.123:5555
 
 # device 3, prisar
 #adb connect 192.168.43.79:5555
